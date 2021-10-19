a9f062d7d5387abedaadb45784def7f3878537b7
gabbyMill <gabbymillatiner@gmail.com>
2021-10-19 20:10:50 +0300

    Versions working with callback functions and ASYNC fs functions (Excluding readdirSync in checkifvalid function

--- findTreasure.js
+++ findTreasure.js
@@ -6,61 +6,108 @@ fs.rmSync(mazeStartingPoint, { recursive: true, force: true });
 
 const { generateMaze } = require("./mazeGenerator/mazeGenerator.js");
 
-function findTreasureSync(roomPath) {
-  if (fs.readFileSync("./map.txt").toString().includes("💰")) {
+function callBack(err, data) {
+  if (err) {
+    console.log("Error: ", err);
     return;
+  } else {
+    return data; // I think this is useless but not sure yet
   }
-  const arrayOfFiles = fs.readdirSync(`${roomPath}`);
-  const contentOfMap = fs.readFileSync(`./map.txt`).toString();
-  arrayOfFiles.forEach(file => {
-    if (
-      path.extname(file) === ".json" &&
-      !contentOfMap.includes(roomPath + file + "/")
-    ) {
-      openChestSync(roomPath + file);
-    } else {
-      findTreasureSync(roomPath + file + "/");
+}
+
+function findTreasure(roomPath) {
+  fs.readFile("./map.txt", (err, contentOfMap) => {
+    if (err) {
+      console.log("Cant read map");
+      return callBack(err); // in all return callbacks below this one
+      // it was also possible to do
+      // callBack(err) // go down a line
+      // return
     }
+    if (contentOfMap.toString().includes("💰")) {
+      return; // treasure found right here baby
+    }
+    fs.readdir(`${roomPath}`, (err, arrayOfFiles) => {
+      if (err) {
+        console.log("Can't open directory");
+        return callBack(err);
+      }
+      fs.readFile(`./map.txt`, (err, contentOfMap) => {
+        if (err) {
+          console.log("Cant read map");
+          return callBack(err);
+        }
+        arrayOfFiles.forEach(file => {
+          if (
+            path.extname(file) === ".json" &&
+            !contentOfMap.includes(roomPath + file + "/")
+          ) {
+            openChest(roomPath + file, function (err) {
+              if (err) {
+                console.log("Failed in opening chest");
+                return callBack(err);
+              }
+            }); // needs callback ?
+          }
+        });
+      });
+    });
   });
 }
 
-function openChestSync(chestPath) {
+function openChest(chestPath) {
   // chest path is the path to a json file
-  const content = fs.readFileSync(chestPath).toString();
-  try {
-    const chest = JSON.parse(content);
-    if (chest.hasOwnProperty("treasure")) {
-      // console.log(`treasure found at ${chestPath}`);
-      drawMapSync(chestPath + "/" + "💰");
-      return;
-    } else {
-      const goodPath = checkIfValidPath(chest);
-      if (goodPath) {
-        drawMapSync(chestPath + "/");
-        findTreasureSync(chest.clue + "/");
+  const content = fs.readFile(chestPath, (err, content) => {
+    if (err) {
+      console.log("Error in opening json file");
+      return callBack(err);
+    }
+    try {
+      const chest = JSON.parse(content);
+      if (chest.hasOwnProperty("treasure")) {
+        // console.log(`treasure found at ${chestPath}`);
+        drawMap(chestPath + "/" + "💰");
+        return;
+      } else {
+        const goodPath = checkIfValidPath(chest);
+        if (goodPath) {
+          drawMap(chestPath + "/");
+          findTreasure(chest.clue + "/");
+        }
+        return;
       }
+    } catch (error) {
+      // not json
       return;
     }
-  } catch (error) {
-    // not json
-    return;
-  }
+  });
 }
 function checkIfValidPath(chest) {
   try {
-    fs.readdirSync(chest.clue + "/");
+    fs.readdirSync(chest.clue + "/"); // haven't changed this yet, maybe don't need to
     return true;
   } catch (error) {
     return false;
   }
 }
 resetMap();
-generateMaze(`${mazeStartingPoint}`, 3, 4, 9);
-findTreasureSync(`${mazeStartingPoint}/`);
-function drawMapSync(path) {
-  const content = fs.readFileSync("./map.txt").toString();
-  fs.writeFileSync("./map.txt", `${content}\n${path}`);
+generateMaze(`${mazeStartingPoint}`, 2, 6, 4);
+findTreasure(`${mazeStartingPoint}/`);
+function drawMap(path) {
+  fs.readFile("./map.txt", (err, content) => {
+    if (err) {
+      console.log("Unable to access map.txt");
+      return callBack(err);
+    }
+    fs.writeFile("./map.txt", `${content}\n${path}`, err => {
+      if (err) {
+        console.log("Problem with map.txt");
+        return callBack(err);
+      }
+    });
+  });
 }
+
 function resetMap(path) {
   fs.writeFileSync("./map.txt", ``);
 }
